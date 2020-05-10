b2963bb4094829d1c6a055a100a23c94808a9b56
root <ivan.bilev@outlook.com>
2020-05-10 09:29:22 -0400

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 echo $TRAVIS_BUILD_DIR
-
-docker build -t ivanb1/multi-client:latest -t ivanb1/multi-client:$SHA -f /home/travis/build/ivbilev/multi-k8s/client/Dockerfile /home/travis/build/ivbilev/multi-k8s/client
-docker build -t ivanb1/multi-server:latest -t ivanb1/multi-server:$SHA -f /home/travis/build/ivbilev/multi-k8s/server/Dockerfile /home/travis/build/ivbilev/multi-k8s/server
-docker build -t ivanb1/multi-worker:latest -t ivanb1/multi-worker:$SHA -f /home/travis/build/ivbilev/multi-k8s/worker/Dockerfile /home/travis/build/ivbilev/multi-k8s/worker
+cd $TRAVIS_BUILD_DIR/client
+docker build -t ivanb1/multi-client:latest -t ivanb1/multi-client:$SHA -f Dockerfile .
+cd $TRAVIS_BUILD_DIR/server
+docker build -t ivanb1/multi-server:latest -t ivanb1/multi-server:$SHA -f Dockerfile .
+cd cd $TRAVIS_BUILD_DIR/worker
+docker build -t ivanb1/multi-worker:latest -t ivanb1/multi-worker:$SHA -f Dockerfile .
 docker push ivanb1/multi-client:latest
 docker push ivanb1/multi-client:$SHA
 docker push ivanb1/multi-server:latest
